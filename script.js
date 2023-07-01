bc5c1e4378835d9590cd13d9b6407ef73912bceb
georgeussher <gussher@icloud.com>
2023-07-01 18:29:47 +0100

    supabase function

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const supabaseUrl = 'https://qorwckgtualpxtpdxuau.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcndja2d0dWFscHh0cGR4dWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyMjI1NjksImV4cCI6MjAwMzc5ODU2OX0.zizLIs6X0hFaU4FZ0WCtg_hCvXuNGznPa67Sia_ogmo';
-const supabase =  supabase.createClient(supabaseUrl, supabaseKey);
+
+const SUPABASE_URL = 'https://qorwckgtualpxtpdxuau.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcndja2d0dWFscHh0cGR4dWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyMjI1NjksImV4cCI6MjAwMzc5ODU2OX0.zizLIs6X0hFaU4FZ0WCtg_hCvXuNGznPa67Sia_ogmo';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
-document.addEventListener('DOMContentLoaded', async() => {
-    try {
-        const { data, error } = await supabase
-        .from('contact')
-        .select('*')
 
-        if (error) {
-            throw error
-        }
+// document.addEventListener('DOMContentLoaded', async() => {
+//     try {
+//         const { data, error } = await supabase
+//         .from('contact')
+//         .select('*')
 
-        console.log(data)
+//         if (error) {
+//             throw error
+//         }
 
-    } catch (error) {
-        console.log('Error fetching contact info:', error)
-    }
+//         console.log(data)
 
-});
+//     } catch (error) {
+//         console.log('Error fetching contact info:', error)
+//     }
+
+// });
