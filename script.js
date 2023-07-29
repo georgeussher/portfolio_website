32934fe68ee77194fde96798cea37276d11dcbcc
georgeussher <gussher@icloud.com>
2023-07-29 15:39:07 +0100

    redirect button added

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-
-const SUPABASE_URL = 'https://qorwckgtualpxtpdxuau.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcndja2d0dWFscHh0cGR4dWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyMjI1NjksImV4cCI6MjAwMzc5ODU2OX0.zizLIs6X0hFaU4FZ0WCtg_hCvXuNGznPa67Sia_ogmo';
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-
-
-// document.addEventListener('DOMContentLoaded', async() => {
-//     try {
-//         const { data, error } = await supabase
-//         .from('contact')
-//         .select('*')
-
-//         if (error) {
-//             throw error
-//         }
-
-//         console.log(data)
-
-//     } catch (error) {
-//         console.log('Error fetching contact info:', error)
-//     }
-
-// });
+//redirects user to the consensus app in a new tab when button is clicked
+function consensusLink(){
+    // window.location.href="https://consensusgpt.netlify.app/"
+    window.open("https://consensusgpt.netlify.app/", "_blank");
+};
